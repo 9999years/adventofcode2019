ed588298eb0826f8f6040dc9322619170a7a5bf9
Rebecca Turner <rbt@sent.as>
2019-12-02 12:30:35 -0500

    Day 2: Part 2 (Racket)

--- day_2_intcode.rkt
+++ day_2_intcode.rkt
@@ -1,10 +1,14 @@
 #lang racket/base
 
+;; Code for Advent of Code 2019, day 2: Program Alarm
+;; https://adventofcode.com/2019/day/2
+
 (require racket/list)
 (require racket/stream)
 (require racket/match)
 (require racket/file)
 (require racket/string)
+(require racket/sequence)
 (require rackunit)
 
 (define OPCODE-ADD 1)
@@ -22,13 +26,12 @@
 (define (process-opcode lst inx)
   (match (drop lst inx)
     [(list opcode lhs-inx rhs-inx out-inx _ ...)
-     (append (take lst out-inx) ; lst until out-inx.
-             ; The newly-calculated value:
-             (list ((opcode->fn opcode)
-                    (list-ref lst lhs-inx)
-                    (list-ref lst rhs-inx)))
-             (drop lst (add1 out-inx)) ; List after new out-inx.
-             )]))
+     (let ([new-value (list ((opcode->fn opcode)
+                             (list-ref lst lhs-inx)
+                             (list-ref lst rhs-inx)))])
+       (append (take lst out-inx)
+               new-value
+               (drop lst (add1 out-inx))))]))
 
 (define (eval-intcode lst [inx 0])
   (if (eq? OPCODE-STOP (list-ref lst inx))
@@ -36,20 +39,43 @@
       (eval-intcode (process-opcode lst inx)
                     (+ inx INSTRUCTION-WIDTH))))
 
-(define (prepare-gravity-assist-intcode lst)
-  (cons
-   (car lst)
-   (append
-    '(12 02)
-    (drop lst 3))))
+;; Replaces elements 1 and 2 in `lst` with the given `noun` and `verb`.
+(define (prepare-program noun verb lst)
+  (append
+   (list (car lst) noun verb)
+   (drop lst 3))) ; 3 being the number of values we replace at the start.
 
-(define (gravity-assist-input)
-  (prepare-gravity-assist-intcode
+;; Gives the program from my input for a given noun and verb.
+(define (program noun verb)
+  (prepare-program
+   noun verb
    (map string->number
         (string-split
          (string-trim
           (file->string "data/day_2_gravity_assist_input.txt"))
          ","))))
+
+;; Computes the output of my program for a given noun and verb.
+(define (program-output noun verb)
+  (car (eval-intcode (program noun verb))))
+
+;; Gives a sequence of all possible nouns and verbs [0..99] inclusive.
+(define (all-inputs)
+  (define possible-input (in-range 100))
+  (apply sequence-append
+         (sequence->list (sequence-map
+                          (λ (noun) (sequence-map
+                                     (λ (i) (list noun i))
+                                     possible-input))
+                          possible-input))))
+
+;; Finds the input noun and verb for a given output value; takes a while to run.
+(define (find-input output)
+  (stream-first
+   (sequence->stream
+    (sequence-filter
+     (λ (input) (= (apply program-output input) output))
+     (all-inputs)))))
 
 (check-equal? (process-opcode '(1 0 0 0) 0) '(2 0 0 0))
 (check-equal? (eval-intcode '(2 3 0 3 99))
@@ -58,4 +84,6 @@
               '(2 4 4 5 99 9801))
 (check-equal? (eval-intcode '(1 1 1 4 99 5 6 0 99))
               '(30 1 1 4 2 5 6 0 99))
-(check-equal? (car (eval-intcode (gravity-assist-input))) 6327510)
+(check-equal? (program-output 12 2) 6327510)
+; This takes a looong time to run (a few minutes).
+(check-equal? (find-input 19690720) '(41 12))
